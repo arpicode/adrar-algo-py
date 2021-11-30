0ad3b7c3ea2ee842373b35bec08c027a7b0b348f
arpicode <arpicode@gmail.com>
2021-11-30 14:47:42 +0100

    fixed answer

--- exo6_4.py
+++ exo6_4.py
@@ -8,6 +8,8 @@
 moyennes = [14.84, 14.14, 16.22, 86, 85, 85, 14.84, 13, 15.85,
             9.99, 12.04, 15.03, 16.22, 12, 84, 10.20, 11.03, 11.03]
 
+moyennes = [note for note in moyennes if 0 <= note <= 20]
+
 moyennes.sort()
 
 print(f'Les 3 meilleurs moyennes : {moyennes[-3:]}')
